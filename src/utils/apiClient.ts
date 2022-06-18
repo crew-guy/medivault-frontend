a2be4f33d456833c243dfc4dc6f3e7d4e369e9bd
ankitsanghvi <ankit.sanghavi87@gmail.com>
2022-06-18 20:01:55 +0530

    update client

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:8080',
+    baseURL: 'https://api.medivault.fallow.in',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
