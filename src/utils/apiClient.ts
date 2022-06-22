cb41662e3232d3fa321406c81ae4442dfce25e29
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2022-06-23 00:49:07 +0530

    recode api

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export const apiClient = axios.create({
-    baseURL: 'https://4vshz4x0r0.execute-api.ap-south-1.amazonaws.com/prod',
+    baseURL: 'https://medivault.fallow.in',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
