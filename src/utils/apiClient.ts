1f126d3505fc5905cd839cbbc12ab6dea1738236
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2022-06-23 00:15:04 +0530

    update endpoint to lambda function

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export const apiClient = axios.create({
-    baseURL: 'https://api.medivault.fallow.in',
+    baseURL: 'https://4vshz4x0r0.execute-api.ap-south-1.amazonaws.com/prod',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
