4cb9b88c570ed6a2711f4f4c5bd8bed938d1917e
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2022-06-22 21:50:23 +0530

    final changes

--- src/components/screens/LoginScreen.tsx
+++ src/components/screens/LoginScreen.tsx
@@ -4,6 +4,7 @@ import cx from 'classnames'
 import { Button } from '@mui/material'
 import { useAuth } from '@contexts/AuthContext'
 import GoogleIcon from '@mui/icons-material/Google';
+import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
 // import APIClient from "./../../APIClient";
 
 const LoginScreen = () => {
@@ -28,12 +29,17 @@ const LoginScreen = () => {
                 <div className="text-content">
                     <div className="title">Log In</div>
                     <div className="sub-title">to start storing</div>
+                    
                 </div>
                 <Button
                     onClick={() => {
                         console.log('clicked', auth)
                         auth.signInWithGoogle()
                     }} endIcon={<GoogleIcon />}>Sign In To Continue</Button>
+                <Button
+                    onClick={() => {
+                        window.location = "https://youtu.be/zBlSC2ViJBM"
+                    }} endIcon={<GoogleIcon />}>How to use app</Button>
             </div>
         </div>
     )
