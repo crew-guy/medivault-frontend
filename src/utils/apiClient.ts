7afa95a0d2bb7d16dc0ccd9de4284643fbc89d60
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2022-06-23 01:00:15 +0530

    hardcode api

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 export const apiClient = axios.create({
-    baseURL: 'https://medivault.fallow.in',
+    baseURL: 'https://api.medivault.fallow.in',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
