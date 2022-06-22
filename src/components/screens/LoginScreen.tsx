2f289e357ed44b42b9658ff092f37579dd097425
Ankit Sanghvi <ankit.sanghavi87@gmail.com>
2022-06-22 21:58:55 +0530

    change icon

--- src/components/screens/LoginScreen.tsx
+++ src/components/screens/LoginScreen.tsx
@@ -38,7 +38,7 @@ const LoginScreen = () => {
                 <Button
                     onClick={() => {
                         (window as any).location = "https://youtu.be/zBlSC2ViJBM" as any
-                    }} endIcon={<GoogleIcon />}>How to use app</Button>
+                    }} endIcon={<PlayCircleFilledWhiteIcon />}>How to use app</Button>
             </div>
         </div>
     )
